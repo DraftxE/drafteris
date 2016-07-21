8ae02f7e154ee6ce48646e646f6797e8c6f2561b
DraftxE <drafterisf2@outlook.com>
2016-07-21 16:36:43 +0700

    Add files via upload

---
+++ beta.c
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std ;
+int main()
+{
+    bool flag=false ;
+    int x = 100 , y = 50 ;
+    cout << "Flag is " << flag << " or " << boolapha << flag endl ;
+    cout << "if x = " << x << " and y = " << y << endl ;
+    cout << "x==y result is " << ( x==y ) << endl ;
+    cout << "x! = y result is " << ( x! = y ) << endl ;
+    cout << "x < y result is " << ( x<y ) << endl ;
+    cout << "x<=y result is " << (x < = y) endl ;
+    cout << "x>y result is " << (x > y) << endl ;
+    cout << "x>=y result is " << (x > = y ) << endl ;
+    system ("pause");
+    return 0 ;
+}
